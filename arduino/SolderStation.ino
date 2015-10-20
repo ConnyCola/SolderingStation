1345ef91ee6a501153f8b89006370c3f05312e82
CSchlipp <christian@schlipp.de>
2015-10-20 23:11:17 +0200

    Switched to ST7735 Display + beautification
    Switched to ST7735 display controller and added some coding style

--- arduino/SolderStation.ino
+++ arduino/SolderStation.ino
@@ -5,14 +5,13 @@
 // Get.A.Soldering.Station@gmail.com
 //*******************************//
 
-
+#define __PROG_TYPES_COMPAT__ 
 #include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_QDTech.h> // Hardware-specific library
+#include <Adafruit_ST7735.h> // Hardware-specific library
 #include <SPI.h>
 
 #include "iron.h"
 #include "stationLOGO.h"
-
 
 #define VERSION "1.5"		//Version der Steuerung
 #define INTRO
@@ -54,14 +53,14 @@
 
 #define PWM_DIV 1024						//default: 64   31250/64 = 2ms
 
-Adafruit_QDTech tft = Adafruit_QDTech(cs_tft, dc, rst);  // Invoke custom library
+Adafruit_ST7735 tft = Adafruit_ST7735(cs_tft, dc, rst);  // Invoke custom library
 
 int pwm = 0; //pwm Out Val 0.. 255
 int soll_temp = 300;
 boolean standby_act = false;
 
 void setup(void) {
-	
+  
 	pinMode(BLpin, OUTPUT);
 	digitalWrite(BLpin, LOW);
 	
@@ -72,53 +71,47 @@ void setup(void) {
 	setPwmFrequency(PWMpin, PWM_DIV);
 	digitalWrite(PWMpin, LOW);
 	
-	tft.init();
+	tft.initR(INITR_BLACKTAB);
 	SPI.setClockDivider(SPI_CLOCK_DIV4);  // 4MHz
 	
-	
 	tft.setRotation(0);	// 0 - Portrait, 1 - Lanscape
-	tft.fillScreen(QDTech_BLACK);
+	tft.fillScreen(ST7735_BLACK);
 	tft.setTextWrap(true);
 	
-	
-	
 	//Print station Logo
-	tft.drawBitmap(2,1,stationLOGO1,124,47,QDTech_GREY);
-	
-	tft.drawBitmap(3,3,stationLOGO1,124,47,QDTech_YELLOW);		
+	tft.drawBitmap(2,1,stationLOGO1,124,47,ST7735_GREY);
+	tft.drawBitmap(3,3,stationLOGO1,124,47,ST7735_YELLOW);		
 	tft.drawBitmap(3,3,stationLOGO2,124,47,Color565(254,147,52));	
 	tft.drawBitmap(3,3,stationLOGO3,124,47,Color565(255,78,0));
 	
 	//BAcklight on
 	digitalWrite(BLpin, HIGH);
 	
-	
 #if defined(INTRO)
-	
 	delay(500);
 	
 	//Print Iron
-	tft.drawBitmap(15,50,iron,100,106,QDTech_GREY);
-	tft.drawBitmap(17,52,iron,100,106,QDTech_YELLOW);
+	tft.drawBitmap(15,50,iron,100,106,ST7735_GREY);
+	tft.drawBitmap(17,52,iron,100,106,ST7735_YELLOW);
 	delay(500);
 	
 	tft.setTextSize(2);
-	tft.setTextColor(QDTech_GREY);
+	tft.setTextColor(ST7735_GREY);
 	tft.setCursor(70,130);
 	tft.print(VERSION);
 	
 	tft.setTextSize(2);
-	tft.setTextColor(QDTech_YELLOW);
+	tft.setTextColor(ST7735_YELLOW);
 	tft.setCursor(72,132);
 	tft.print(VERSION);
 	
 	tft.setTextSize(1);
-	tft.setTextColor(QDTech_GREY);
+	tft.setTextColor(ST7735_GREY);
 	tft.setCursor(103,0);
 	tft.print("v");
 	tft.print(VERSION);
 	
-	tft.setTextColor(QDTech_YELLOW);
+	tft.setTextColor(ST7735_YELLOW);
 	tft.setCursor(104,1);
 	tft.print("v");
 	tft.print(VERSION);
@@ -126,9 +119,8 @@ void setup(void) {
 	delay(2500);
 #endif
 	
-	
-	tft.fillRect(0,47,128,125,QDTech_BLACK);
-	tft.setTextColor(QDTech_WHITE);
+	tft.fillRect(0,47,128,125,ST7735_BLACK);
+	tft.setTextColor(ST7735_WHITE);
 
 	tft.setTextSize(1);
 	tft.setCursor(1,84);
@@ -154,7 +146,6 @@ void setup(void) {
 	tft.print("pwm");
 	
 	tft.setTextSize(2);
-	
 }
 
 void loop() {
@@ -162,28 +153,26 @@ void loop() {
 	int actual_temperature = getTemperature();
 	soll_temp = map(analogRead(POTI), 0, 1024, 0, MAX_POTI);
 	
-	
 	//TODO: Put in Funktion
 	tft.setCursor(2,55);
-	if (digitalRead(STANDBYin) == true)
-		tft.setTextColor(QDTech_BLACK);
-	else
-		tft.setTextColor(QDTech_WHITE);
+	if (digitalRead(STANDBYin) == true) {
+		tft.setTextColor(ST7735_BLACK);
+	} else {
+		tft.setTextColor(ST7735_WHITE);
+	}
 	tft.print("SB");
-	
-	//
 	
 	int soll_temp_tmp = soll_temp;
 	
-	if (digitalRead(STANDBYin) == false)
+	if (digitalRead(STANDBYin) == false) {
 		standby_act = true;
-	else
+	} else {
 		standby_act = false;
+	}
 	
-	if (standby_act && (soll_temp >= STANDBY_TEMP ))
+	if (standby_act && (soll_temp >= STANDBY_TEMP )) {
 		soll_temp_tmp = STANDBY_TEMP;
-	
-	
+	}
 	
 	int diff = (soll_temp_tmp + OVER_SHOT)- actual_temperature;
 	pwm = diff*CNTRL_GAIN;
@@ -196,33 +185,30 @@ void loop() {
 	//8 Bit Range
 	pwm = pwm > MAX_PWM ? pwm = MAX_PWM : pwm < 0 ? pwm = 0 : pwm;
 	
-	
-	
 	//NOTfall sicherheit / Spitze nicht eingesteckt
 	if (actual_temperature > 550){
 		pwm = 0;
 		actual_temperature = 0;
 	}
 	
-	
 	analogWrite(PWMpin, pwm);
 	//digitalWrite(PWMpin, LOW);
 	
 	writeHEATING(soll_temp, actual_temperature, pwm);
-	
+ 
 	delay(DELAY_MAIN_LOOP);		//wait for some time
 }
 
 
 int getTemperature()
 {
-  analogWrite(PWMpin, 0);		//switch off heater
-  delay(DELAY_MEASURE);			//wait for some time (to get low pass filter in steady state)
-  int adcValue = analogRead(TEMPin); // read the input on analog pin 7:
-  Serial.print("ADC Value ");
-  Serial.print(adcValue);
-  analogWrite(PWMpin, pwm);	//switch heater back to last value
-  return round(((float) adcValue)*ADC_TO_TEMP_GAIN+ADC_TO_TEMP_OFFSET); //apply linear conversion to actual temperature
+	analogWrite(PWMpin, 0);		//switch off heater
+	delay(DELAY_MEASURE);			//wait for some time (to get low pass filter in steady state)
+	int adcValue = analogRead(TEMPin); // read the input on analog pin 7:
+	Serial.print("ADC Value ");
+	Serial.print(adcValue);
+	analogWrite(PWMpin, pwm);	//switch heater back to last value
+	return round(((float) adcValue)*ADC_TO_TEMP_GAIN+ADC_TO_TEMP_OFFSET); //apply linear conversion to actual temperature
 }
 
 
@@ -231,7 +217,6 @@ int getTemperature()
 
 void writeHEATING(int tempSOLL, int tempVAL, int pwmVAL){
 	static int d_tempSOLL = 2;		//Tiefpass für Anzeige (Poti zittern)
-
 	static int tempSOLL_OLD = 	10;
 	static int tempVAL_OLD	= 	10;
 	static int pwmVAL_OLD	= 	10;
@@ -242,36 +227,41 @@ void writeHEATING(int tempSOLL, int tempVAL, int pwmVAL){
 	tft.setTextSize(5);
 	if (tempVAL_OLD != tempVAL){
 		tft.setCursor(30,57);
-		tft.setTextColor(QDTech_BLACK);
+		tft.setTextColor(ST7735_BLACK);
 		//tft.print(tempSOLL_OLD);
 		//erste Stelle unterschiedlich
 		if ((tempVAL_OLD/100) != (tempVAL/100)){
 			tft.print(tempVAL_OLD/100);
+		} else {
+			tft.print(" ");
 		}
-		else
-			tft.print(" ");
 		
-		if ( ((tempVAL_OLD/10)%10) != ((tempVAL/10)%10) )
+		if ( ((tempVAL_OLD/10)%10) != ((tempVAL/10)%10) ) {
 			tft.print((tempVAL_OLD/10)%10 );
-		else
+		} else {
 			tft.print(" ");
+		}
 		
-		if ( (tempVAL_OLD%10) != (tempVAL%10) )
+		if ( (tempVAL_OLD%10) != (tempVAL%10) ) {
 			tft.print(tempVAL_OLD%10 );
+		}
 		
 		tft.setCursor(30,57);
-		tft.setTextColor(QDTech_WHITE);
+		tft.setTextColor(ST7735_WHITE);
 		
-		if (tempVAL < 100)
+		if (tempVAL < 100) {
 			tft.print(" ");
-		if (tempVAL <10)
+		}
+		if (tempVAL <10) {
 			tft.print(" ");
+		}
 		
 		int tempDIV = round(float(tempSOLL - tempVAL)*8.5);
 		tempDIV = tempDIV > 254 ? tempDIV = 254 : tempDIV < 0 ? tempDIV = 0 : tempDIV;
 		tft.setTextColor(Color565(tempDIV, 255-tempDIV, 0));
-		if (standby_act)
-			tft.setTextColor(QDTech_CYAN);
+		if (standby_act) {
+			tft.setTextColor(ST7735_CYAN);
+		}
 		tft.print(tempVAL);
 		
 		tempVAL_OLD = tempVAL;
@@ -280,30 +270,34 @@ void writeHEATING(int tempSOLL, int tempVAL, int pwmVAL){
 	//if (tempSOLL_OLD != tempSOLL){
 	if ((tempSOLL_OLD+d_tempSOLL < tempSOLL) || (tempSOLL_OLD-d_tempSOLL > tempSOLL)){
 		tft.setCursor(30,102);
-		tft.setTextColor(QDTech_BLACK);
+		tft.setTextColor(ST7735_BLACK);
 		//tft.print(tempSOLL_OLD);
 		//erste Stelle unterschiedlich
 		if ((tempSOLL_OLD/100) != (tempSOLL/100)){
 			tft.print(tempSOLL_OLD/100);
+		} else {
+			tft.print(" ");
 		}
-		else
-			tft.print(" ");
 		
-		if ( ((tempSOLL_OLD/10)%10) != ((tempSOLL/10)%10) )
+		if ( ((tempSOLL_OLD/10)%10) != ((tempSOLL/10)%10) ) {
 			tft.print((tempSOLL_OLD/10)%10 );
-		else
+		} else {
 			tft.print(" ");
+		}
 		
-		if ( (tempSOLL_OLD%10) != (tempSOLL%10) )
+		if ( (tempSOLL_OLD%10) != (tempSOLL%10) ) {
 			tft.print(tempSOLL_OLD%10 );
+		}
 		
 		//Neuen Wert in Weiß schreiben
 		tft.setCursor(30,102);
-		tft.setTextColor(QDTech_WHITE);
-		if (tempSOLL < 100)
+		tft.setTextColor(ST7735_WHITE);
+		if (tempSOLL < 100) {
 			tft.print(" ");
-		if (tempSOLL <10)
+		}
+		if (tempSOLL <10) {
 			tft.print(" ");
+		}
 		
 		tft.print(tempSOLL);
 		tempSOLL_OLD = tempSOLL;
@@ -314,73 +308,78 @@ void writeHEATING(int tempSOLL, int tempVAL, int pwmVAL){
 	tft.setTextSize(2);
 	if (pwmVAL_OLD != pwmVAL){
 		tft.setCursor(80,144);
-		tft.setTextColor(QDTech_BLACK);
+		tft.setTextColor(ST7735_BLACK);
 		//tft.print(tempSOLL_OLD);
 		//erste stelle Unterscheidlich
 		if ((pwmVAL_OLD/100) != (pwmVAL/100)){
 			tft.print(pwmVAL_OLD/100);
+		} else {
+			tft.print(" ");
 		}
-		else
-			tft.print(" ");
 		
-		if ( ((pwmVAL_OLD/10)%10) != ((pwmVAL/10)%10) )
+		if ( ((pwmVAL_OLD/10)%10) != ((pwmVAL/10)%10) ) {
 			tft.print((pwmVAL_OLD/10)%10 );
-		else
+		} else {
 			tft.print(" ");
+		}
 		
-		if ( (pwmVAL_OLD%10) != (pwmVAL%10) )
+		if ( (pwmVAL_OLD%10) != (pwmVAL%10) ) {
 			tft.print(pwmVAL_OLD%10 );
+		}
 		
 		tft.setCursor(80,144);
-		tft.setTextColor(QDTech_WHITE);
-		if (pwmVAL < 100)
+		tft.setTextColor(ST7735_WHITE);
+		if (pwmVAL < 100) {
 			tft.print(" ");
-		if (pwmVAL <10)
+		}
+		if (pwmVAL <10) {
 			tft.print(" ");
+		}
 		
 		tft.print(pwmVAL);
 		pwmVAL_OLD = pwmVAL;
 		
 	}
-	
 }
 
 
 
 
 uint16_t Color565(uint8_t r, uint8_t g, uint8_t b) {
-  return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
+	return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 }
 
 
 
 void setPwmFrequency(int pin, int divisor) {
-  byte mode;
-  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 64: mode = 0x03; break;
-      case 256: mode = 0x04; break;
-      case 1024: mode = 0x05; break;
-      default: return;
-    }
-    if(pin == 5 || pin == 6) {
-      TCCR0B = TCCR0B & 0b11111000 | mode;
-    } else {
-      TCCR1B = TCCR1B & 0b11111000 | mode;
-    }
-  } else if(pin == 3 || pin == 11) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 32: mode = 0x03; break;
-      case 64: mode = 0x04; break;
-      case 128: mode = 0x05; break;
-      case 256: mode = 0x06; break;
-      case 1024: mode = 0x7; break;
-      default: return;
-    }
-    TCCR2B = TCCR2B & 0b11111000 | mode;
-  }
+	byte mode;
+	if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+		switch(divisor) {
+			case 1: 		mode = 0x01; break;
+			case 8: 		mode = 0x02; break;
+			case 64: 		mode = 0x03; break;
+			case 256: 	mode = 0x04; break;
+			case 1024: 	mode = 0x05; break;
+			default: return;
+		}
+		
+		if(pin == 5 || pin == 6) {
+			TCCR0B = TCCR0B & 0b11111000 | mode;
+		} else {
+			TCCR1B = TCCR1B & 0b11111000 | mode;
+		}
+	} else if(pin == 3 || pin == 11) {
+		switch(divisor) {
+			case 1: 		mode = 0x01; break;
+			case 8: 		mode = 0x02; break;
+			case 32: 		mode = 0x03; break;
+			case 64: 		mode = 0x04; break;
+			case 128: 	mode = 0x05; break;
+			case 256: 	mode = 0x06; break;
+			case 1024: 	mode = 0x07; break;
+			default: return;
+		}
+		
+		TCCR2B = TCCR2B & 0b11111000 | mode;
+	}
 }
